a56e9096b5bc9e7e2e621b2dd9d758b1d3a5b865
남정욱 <wjddnr3304@gmail.com>
2024-11-05 11:21:31 +0900

    초기 커밋.

---
+++ buyinfo_ex/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace buyinfo_ex
+{
+    struct buy
+    {
+        public string company; // 구매처
+        public string item; // 구매품목
+        public int qty; // 구매량
+        public bool complete; // 입고 여부
+    }
+    class Program
+    {
+        // 일별 구매계획 등록
+        // 특정 구매건에 대해 입고되었음을 저장
+        //품목별로 구매계획과 입고량을 화면에 출력
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
